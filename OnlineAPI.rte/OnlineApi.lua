51c0fe277ff97f650e9db0c0d10d8837ef18a7be
Chicken man <super.saga@hotmail.com>
2013-09-21 21:53:59 -0400

    Something

--- OnlineAPI.rte/OnlineApi.lua
+++ OnlineAPI.rte/OnlineApi.lua
@@ -1,15 +1,31 @@
-local OnlineApi = {}
+OnlineApi = {}
 
-Singleton = {};
-
-function OnlineApi.getinstance()
-	if not Singleton then
-		setmetatable(OnlineApi, {__metatable = function() print("This is a locked metatable") end}
-		Singleton = OnlineApi
-	end
-	return Singleton
-end
+local Singleton = {};
 
 function OnlineApi:new()
 	print("This is a singleton class! You cannot make a new OnlineAPI instance, use getinstance() instead.")
 end
+
+function OnlineApi:registeractor(actor)
+	
+end
+
+function OnlineApi:forceupdate()
+	
+end
+
+
+
+function OnlineApi:getinstance()
+	return Singleton
+end
+
+setmetatable(OnlineApi, {
+	
+	__metatable = function() print("This is a locked metatable.") end
+	
+	__newindex = function() print("You may not extend this table.") end
+	
+}
+
+Singleton = OnlineApi
