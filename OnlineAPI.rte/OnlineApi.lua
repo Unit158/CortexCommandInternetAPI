edf52c13648aedfdd36b63aba49c85a13bf4fdec
Chicken man <super.saga@hotmail.com>
2013-09-23 19:31:28 -0400

    Updated the client side API.

--- OnlineAPI.rte/OnlineApi.lua
+++ OnlineAPI.rte/OnlineApi.lua
@@ -1,15 +1,47 @@
-local OnlineApi = {}
+OnlineApi = {}
 
-Singleton = {};
-
-function OnlineApi.getinstance()
-	if not Singleton then
-		setmetatable(OnlineApi, {__metatable = function() print("This is a locked metatable") end}
-		Singleton = OnlineApi
-	end
-	return Singleton
-end
+local Singleton = {};
 
 function OnlineApi:new()
 	print("This is a singleton class! You cannot make a new OnlineAPI instance, use getinstance() instead.")
 end
+
+--[[
+	Registers an AHuman with the API. Will check that the server has the same AHuman.
+	Requires: An a pointer to an AHuman object
+	Returns: The pointer to the api's object.
+]]--
+
+function OnlineApi:registerAHuman(AHuman)
+	
+end
+
+--[[
+	Registers an HDFirearm with the API. Will check that the server has the same HDFirearm.
+	Requires: An a pointer to an HDFirearm object
+	Returns: The pointer to the api's object.
+]]--
+
+function OnlineApi:registerHDFirearm(HDFirearm)
+	
+end
+
+function OnlineApi:forceupdate()
+	
+end
+
+
+
+function OnlineApi:getinstance()
+	return Singleton
+end
+
+setmetatable(OnlineApi, {
+	
+	__metatable = function() print("This is a locked metatable.") end
+	
+	__newindex = function() print("You may not extend this table.") end
+	
+}
+
+Singleton = OnlineApi
